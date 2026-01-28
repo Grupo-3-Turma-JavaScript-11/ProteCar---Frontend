22ab831eb2f4a0ce319c6158a5192b80aedebb65
Carolina Figueiredo <carolinasfigueiredo@hotmail.com>
2026-01-28 16:29:20 -0300

    Criação Componente Postagem

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ListaSeguros from './components/seguro/listaseguros/ListaSeguros'
+import FormSeguro from './components/seguro/formseguro/FormSeguro'
+import DeletarSeguro from './components/seguro/deletarseguro/DeletarSeguro'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			{/* <AuthProvider> */}
+				<BrowserRouter>
+					{/* <Navbar /> */}
+					<div className="min-h-[80vh]">
+						<Routes>
+							{/* <Route path="/" element={<Login />} /> */}
+              {/* <Route path="/home" element={<Home />} /> */}
+							{/* <Route path="/cadastro"	element={<Cadastro />}/> */}
+              <Route path="/" element={<ListaSeguros />} />
+							<Route path="/categorias" element={<ListaCategorias/>} />
+							<Route path="/cadastrarcategoria" element={<FormCategoria />} />
+							<Route path="/editarcategoria/:id" element={<FormCategoria />} />
+							<Route path="/deletarcategoria/:id" element={<DeletarCategoria />} />
+							<Route path="/produtos" element={<ListaSeguros />} />
+              <Route path="/cadastrarproduto" element={<FormSeguro />} />
+              <Route path="/editarproduto/:id" element={<FormSeguro />} />
+              <Route path="/deletarseguro/:id" element={<DeletarSeguro />} />
+						</Routes>
+					</div>
+					{/* <Footer /> */}
+				</BrowserRouter>
+			{/* </AuthProvider> */}
+		</>
+	)
 }
 
 export default App
---
+++ src/components/seguro/listaseguros/ListaSeguros.tsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+// import { useNavigate } from "react-router-dom";
+import { SyncLoader } from "react-spinners";
+import { buscar } from "../../../service/Service";
+import type Produto from "../../../models/Produto";
+import CardSeguro from "../cardseguro/CardSeguro";
+
+function ListaSeguros() {
+
+    // const navigate = useNavigate();
+
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+
+    const [produtos, setProdutos] = useState<Produto[]>([])
+
+    // const { usuario, handleLogout } = useContext(AuthContext)
+    // const token = usuario.token
+
+    // useEffect(() => {
+    //     if (token === '') {
+    //         alert('Você precisa estar logado!')
+    //         navigate('/')
+    //     }
+    // }, [token])
+
+    useEffect(() => {
+        buscarProdutos()    
+    }, [produtos.length])
+
+    async function buscarProdutos() {
+        try {
+
+            setIsLoading(true)
+
+            await buscar('/produtos', setProdutos, {
+                // headers: { Authorization: token }
+            })
+        } catch (error: any) {
+            if (error.toString().includes('401')) {
+                // handleLogout()
+            }
+        }finally {
+            setIsLoading(false)
+        }
+    }
+
+    return (
+        <>
+
+            {isLoading && (
+                <div className="flex justify-center w-full my-8">
+                    <SyncLoader
+                        color="#312e81"
+                        size={32}
+                    />
+                </div>
+            )}
+
+            <div className="flex justify-center w-full my-4">
+                <div className="container flex flex-col">
+
+                    {(!isLoading && produtos.length === 0) && (
+                            <span className="text-3xl text-center my-8">
+                                Nenhum Produto foi encontrado!
+                            </span>
+                    )}
+
+                    <div className="grid grid-cols-1 md:grid-cols-2 
+                                    lg:grid-cols-3 gap-8">
+                            {
+                                produtos.map((produto) => (
+                                    <CardSeguro key={produto.id} produto={produto}/>
+                                ))
+                            }
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ListaSeguros;
